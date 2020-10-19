ec3b763860dc20dcc74b8dad94936b525287952c
dan63047 <dan63047@gmail.com>
2020-10-19 22:00:00 +0300

    C is weird

---
+++ C/cyberbullying_math.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> 
+
+int main(){
+    int a, b, sum, answer;
+    bool error;
+    error = false;
+    while (!error)
+    {
+        a = rand() % 100;
+        b = rand() % 100;
+        sum = a + b;
+        printf("%d + %d = ", a, b);
+        scanf("%d", &answer);
+        if (answer != sum){
+            error = true;
+        }
+    }
+    printf("LOX!!!\n");
+}
